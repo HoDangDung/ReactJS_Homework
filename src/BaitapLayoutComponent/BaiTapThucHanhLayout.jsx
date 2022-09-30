c7ce59e0808f15385a05d4c172632147128a437e
Hồ Đăng Dũng <65653599+HoDangDung@users.noreply.github.com>
2022-09-30 18:51:03 +0700

    done

--- src/BaitapLayoutComponent/BaiTapThucHanhLayout.jsx
+++ src/BaitapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './Header'
 import Body from './Body'
 import Footer from './Footer'
-import "./layout.css";
 
 const BaiTapThucHanhLayout = () => {
   return (
